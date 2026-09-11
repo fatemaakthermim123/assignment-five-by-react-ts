27b1a4cb306a00ed47b36d9ea60ab180190cfda0
fatemaakthermim123 <fatema3642@gmail.com>
2026-09-11 12:47:09 +0600

    banner done

---
+++ src/Banner/Banner.tsx
@@ -0,0 +1,32 @@
+ import React from 'react';
+ import bannerLogo from "../assets/banner-stack.png"
+ const Banner = () => {
+    return (
+        <div className='flex justify-between items-center max-w-6xl mx-auto'>
+            {/* leftside */}
+    <div className=''>
+         <h1 className='mb-7 font-extrabold text-4xl'>Build Your Ideal<br/>
+          <span className='bg-linear-to-r  from-[#FF5722] to-[#7C3AED] 
+          bg-clip-text text-transparent'>Development Stack</span></h1>
+         <p className='mb-7'>Explore frontend, backend, database, and tooling options,<br/>
+       compare them side by side, and put together the stack that fits your<br/>
+          next project.</p>
+         <div className='flex gap-3'>
+            <button className='text-white  rounded-xl w-44 h-10 bg-linear-to-r 
+             from-[#FF5722] to-[#EC4899]
+            hover:scale-105 transition duration-200'>Explore Technologies</button>
+            <button className="rounded-xl w-44 h-10 bg-amber-50 border-amber-50
+             hover:bg-gray-100 hover:scale-105 transition duration-200"
+          >Learn more</button>
+            </div>
+            </div>
+                 {/* rightside */}
+     <div>
+          <img className='' src={bannerLogo}/>
+     </div>
+
+        </div>
+    );
+ };
+ 
+ export default Banner;
